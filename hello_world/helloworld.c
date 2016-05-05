9592daf485b325c1b132c51cd3f21680efcf75a0
Dan McClory <danmcclory@gmail.com>
2016-05-05 16:56:04 -0400

    update helloworld.c

--- hello_world/helloworld.c
+++ hello_world/helloworld.c
@@ -3,5 +3,6 @@
 int main() {
   int x = 200;
   int y = 300;
-  printf("Hello World! Also here's a number: %i\n", x + y);
+  printf("Hello World!\n");
+  printf("Also here's a number: %i\n", x + y);
 }
